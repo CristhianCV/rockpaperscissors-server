1e009260f4e8f7ce55a86f5cbf89b75d6879b7a1
CRISTHIAN <cristhiancv45@gmail.com>
2020-05-31 21:57:27 -0500

    feature(real-tim): create room and verify room events created

--- server/main.js
+++ server/main.js
@@ -9,12 +9,54 @@ app.use(cors());
 var roomsInfo = new Map();
 const types = { ROCK: "rock", SCISSOR: "scissors", PAPER: "paper" };
 
-function showOnConsola(message) {
-  console.log(message);
-}
+const GAME_EVENTS = {
+  CREATE_ROOM: "CREATE_ROOM",
+  ROOM_CREATED: "ROOM_CREATED",
+  VERIFY_ROOM_AVAILABILITY: "VERIFY_ROOM_AVAILABILITY",
+  VERIFY_ROOM_AVAILABILITY_RESULT: "VERIFY_ROOM_AVAILABILITY_RESULT",
+  JOIN_ROOM: "JOIN_ROOM",
+  GAME_IS_READY: "GAME_IS_READY",
+  USER_PICKED: "USER_PICKED",
+  GAME_RESULT: "GAME_RESULT",
+  PLAY_AGAIN_REQUEST: "PLAY_AGAIN_REQUEST",
+  PLAY_AGAIN: "PLAY_AGAIN",
+  EXIT_GAME: "EXIT_GAME",
+};
 
 io.on("connection", function (socket) {
-  socket.on("joinRoom", (data) => {
+  socket.on(GAME_EVENTS.CREATE_ROOM, (hostName) => {
+    let roomId = getRandomId();
+    let hostId = getRandomId();
+
+    roomsInfo.set(roomId, new Map());
+    roomsInfo
+      .get(roomId)
+      .set(hostId, { id: hostId, name: hostName, typePicked: "", score: 0 });
+    socket.join(roomId);
+
+    socket.emit(GAME_EVENTS.ROOM_CREATED, {
+      error: "",
+      data: { roomId: roomId, hostId: hostId },
+    });
+    console.log("create room " + roomId);
+  });
+
+  socket.on(GAME_EVENTS.VERIFY_ROOM_AVAILABILITY, (room) => {
+    if (!roomsInfo.has(room) || roomsInfo.get(room).size === 0) {
+      socket.emit(GAME_EVENTS.VERIFY_ROOM_AVAILABILITY_RESULT, {
+        error: "The room isn't available.",
+        data: null,
+      });
+    } else {
+      socket.emit(GAME_EVENTS.VERIFY_ROOM_AVAILABILITY_RESULT, {
+        error: "",
+        data: null,
+      });
+    }
+    console.log("verify room");
+  });
+
+  socket.on(GAME_EVENTS.JOIN_ROOM, (data) => {
     const { id, name, score, typePicked, isWinner, room } = data;
 
     if (!roomsInfo.has(room)) {
@@ -38,7 +80,7 @@ io.on("connection", function (socket) {
       showOnConsola("connected" + id);
 
       if (roomsInfo.get(room).size === 2) {
-        io.sockets.to(room).emit("gameReady", {
+        io.sockets.to(room).emit(GAME_EVENTS.GAME_IS_READY, {
           code: "OK",
           message: "The game is ready to start",
         });
@@ -47,7 +89,7 @@ io.on("connection", function (socket) {
     }
   });
 
-  socket.on("circleSelected", function (data) {
+  socket.on(GAME_EVENTS.USER_PICKED, function (data) {
     const { id, name, score, typePicked, isWinner, room } = data;
 
     roomsInfo.get(room).get(id).typePicked = typePicked;
@@ -66,10 +108,9 @@ io.on("connection", function (socket) {
       let winnerIndex = getWinner(playerOneSelectedType, playerTwoSelectedType);
       showOnConsola("winnerIndex" + winnerIndex);
       if (winnerIndex) {
-        roomsInfo.get(room).get(idPlayers[winnerIndex - 1]).isWinner = true;
         roomsInfo.get(room).get(idPlayers[winnerIndex - 1]).score += 1;
       }
-      io.sockets.to(room).emit("circleSelectedResponse", {
+      io.sockets.to(room).emit(GAME_EVENTS.GAME_RESULT, {
         idWinner: winnerIndex
           ? roomsInfo.get(room).get(idPlayers[winnerIndex - 1]).id
           : 0,
@@ -79,27 +120,35 @@ io.on("connection", function (socket) {
     }
   });
 
-  socket.on("playAgain", function (data) {
+  socket.on(GAME_EVENTS.PLAY_AGAIN_REQUEST, function (data) {
     const { id, name, score, typePicked, isWinner, room } = data;
 
     roomsInfo.get(room).get(id).typePicked = "";
-    roomsInfo.get(room).get(id).isWinner = false;
 
-    roomsInfo.get(roomsInfo.get(room).get(id));
+    let idPlayers = Array.from(roomsInfo.get(room).keys());
+
+    if (
+      roomsInfo.get(room).size === 2 &&
+      roomsInfo.get(room).get(idPlayers[0]).typePicked.trim() !== "" &&
+      roomsInfo.get(room).get(idPlayers[1]).typePicked.trim() !== ""
+    ) {
+      io.sockets.to(room).emit(GAME_EVENTS.PLAY_AGAIN);
+      showOnConsola("reload");
+    }
   });
 
   socket.on("disconnect", function (data) {
     showOnConsola("disconnect");
   });
 
-  socket.on("exitGame", (data) => {
+  socket.on(GAME_EVENTS.EXIT_GAME, (data) => {
     const { id, name, score, typePicked, isWinner, room } = data;
 
-    if (roomsInfo.has(room) && roomsInfo.get(room).size > 0) {
+    if (roomsInfo.has(room) && roomsInfo.get(room).has(id)) {
       roomsInfo.get(room).delete(id);
     }
 
-    showOnConsola("exitGame" + id);
+    showOnConsola(GAME_EVENTS.EXIT_GAME + id);
   });
 });
 
@@ -156,6 +205,14 @@ const getWinner = (playerOneSelectedType, playerTwoSelectedType) => {
   }
   return playerWinner;
 };
+
+function showOnConsola(message) {
+  console.log(message);
+}
+
+function getRandomId() {
+  return Math.random().toString(36).substr(2, 9);
+}
 
 server.listen(8080, function () {
   console.log("Servidor corriendo en http://localhost:8080");
